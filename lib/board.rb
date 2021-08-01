ffae5326c0ee46f1dbf8fab7d80929b8b266a65a
Noak Pettersson <noake12@gmail.com>
2021-08-01 21:47:58 +0200

    added print method to board

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,10 @@
 
 # class for the board
 class Board
+  @@WINNING_BOARDS = [1, 2, 3], [4, 5, 6], [7, 8, 9],
+                     [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                     [1, 5, 9], [3, 5, 7]
+
   def initialize
     @board = [
       [1, 2, 3],
@@ -10,16 +14,23 @@ class Board
     ]
   end
 
-  @@WINNING_BOARDS = [1, 2, 3], [4, 5, 6], [7, 8, 9],
-                     [1, 4, 7], [2, 5, 8], [3, 6, 9],
-                     [1, 5, 9], [3, 5, 7]
   def winning?
     @board.each do |sub_array|
       return true if @@WINNING_BOARDS.include?(sub_array)
     end
     false
   end
+
+  def print_board
+    @board.each do |row|
+      row.each do |val|
+        print "#{val} "
+      end
+      print "\n"
+    end
+  end
 end
 
 b = Board.new
 puts b.winning?
+b.print_board
